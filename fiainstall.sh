a9ce212f90a40ca7e7f127a9d137dd38e9c2140b
Vikas Swami <vikasbswami@gmail.com>
2021-08-20 23:42:00 +0530

    vmware install complete

--- fiainstall.sh
+++ fiainstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cmd=(whiptail --separate-output --checklist "Select apps to install:\n(Use ARROWs, TAB, SPACE and ENTER keys)" 22 56 10)
 options=(
-1 "VMWare" off
+1 "VMWare Player" off
 2 "Virtual Box" off
 3 "Google Chrome" off
 4 "Filezilla Client" off
@@ -15,6 +15,9 @@ options=(
 12 "Inkscape" off)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 
+mkdir fia_temp
+cd fia_temp
+sudo whoami >> /dev/null
 clear
 
 checknet() {
@@ -43,14 +46,65 @@ update() {
 	sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 	if [[ $? -eq 0 ]]
 	then
+		tput setaf 3
+		echo "Updating packages..."
+		tput cuu1
+		tput cuf 21
 		tput setaf 2
 		echo "[Done!]"
 		tput sgr0
 	else
+		tput setaf 3
+		echo "Updating packages..."
+		tput cuu1
+		tput cuf 21
 		tput setaf 1
-		echo "[Update Error. Exiting!]"
+		echo "[Failed. Exiting!]"
 		tput sgr0
 		exit
+	fi
+}
+
+install_vmware()
+{
+	echo "Installing VMWare Player..."
+        sudo apt install build-essential linux-headers-`uname -r` -y
+	if [[ $? -ne 0 ]]
+	then
+		tput setaf 1
+		echo "Linux headers cannot be installed."
+		tput setaf 3
+		echo "Installing VMWare Player..."
+		tput cuu1
+		tput cuf 28
+		tput setaf 1
+		echo "[Failed.]"
+		return
+	fi
+
+	wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" https://www.vmware.com/go/getplayer-linux
+	if [[ $? -ne 0 ]]
+	then
+		tput setaf 1
+                echo "VMWare Player cannot be downloaded."
+                tput setaf 3
+                echo "Installing VMWare Player..."
+                tput cuu1
+                tput cuf 28
+                tput setaf 1
+                echo "[Failed.]"
+                return
+	else
+		chmod +x getplayer-linux
+		sudo ./getplayer-linux --required --eulas-agreed
+		tput setaf 3
+                echo "Installing VMWare Player..."
+                tput cuu1
+                tput cuf 28
+                tput setaf 2
+                echo "[Done!]"
+                return
+
 	fi
 }
 
@@ -61,7 +115,7 @@ for choice in $choices
 do
 	case $choice in
 	1)
-		echo "vmware"
+		install_vmware 
 		;;
 	2)
 		echo "virtualbox"
